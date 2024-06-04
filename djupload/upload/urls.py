57fef4dec24d7f43724aa3cf01707dbcdc62f47a
Locatelli Alex <locatelli.alexgiannino.studente@itispaleocapa.it>
2024-06-04 10:21:38 +0200

    Added basic auth system

--- djupload/upload/urls.py
+++ djupload/upload/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home", views.home, name="home"),
     path("about", views.about, name="about"),
-    path("login", views.login, name="login"),
+    path('login', views.user_login, name='login'),
+    path('signup', views.user_signup, name='signup'),
+    path('logout', views.user_logout, name='logout'),
 ]
--- djupload/upload/views.py
+++ djupload/upload/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
 def index(request):
@@ -13,5 +15,33 @@ def about(request):
     return render(request, "upload/about.html")
 
 
-def login(request):
-    return render(request, "upload/login.html")
+# signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('upload:login')
+    else:
+        form = SignupForm()
+    return render(request, 'upload/signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('upload:home')
+    else:
+        form = LoginForm()
+    return render(request, 'upload/login.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('upload:index')
